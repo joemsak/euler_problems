fc73227bee312cec5dadeb18dadfedba8d9a8674
Joe Sak <joe@joesak.com>
2012-05-06 19:29:37 -0500

    Solve 3rd problem?

---
+++ 03/largest_prime_factor_of_the_number_600_851_475_143.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/ruby
+class Fixnum
+  def prime?
+    factors == [self, 1]
+  end
+
+  def factors
+    return [1] if self == 1
+    i = 2
+    factor = (i..self).find { |n| self % n == 0 }
+    [factor] + (self / factor).factors
+  end
+end
+
+factors = 600_851_475_143.factors
+prime_factors = factors.delete_if { |n| !n.prime? }
+
+p largest_prime_factor = prime_factors.max
