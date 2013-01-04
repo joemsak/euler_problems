85a43ce41eccd0a147b0e6dc19e535fd257df617
Joe Sak <joe@joesak.com>
2013-01-03 23:00:58 -0600

    Optimize problem 5

--- 05/smallest_multiple.rb
+++ 05/smallest_multiple.rb
@@ -1,12 +1,32 @@
 #!/usr/bin/ruby
 
-def all_evenly_divisible?(m)
-  (2..20).all? { |n| m % n == 0 }
-end
+# SURE We could just check (1..20)
+# But we want this shit to go faster
+#
+# So:
+#
+# all numbers are evenly divisible by 1
+# leave it out
+#
+# if it's evenly divisible by 20
+# then it is " " by 2, 4, 5, 10, & 15
+#
+# if it is evenly divisible by 18
+# then it is " " by 3, 6, & 9
+#
+# if it is evenly divisible by 16
+# then it is " " by 8
+#
+# if it is evenly divisible by 14
+# then it is " " by 7
+#
+# finally, leave the prime numbers in
 
-multiple = 2540
+factors = [11, 12, 13, 14, 16, 17, 18, 19, 20]
 
-until all_evenly_divisible?(multiple)
+multiple = 20
+
+until factors.all? { |n| multiple % n == 0 }
   multiple += 20
 end
 
