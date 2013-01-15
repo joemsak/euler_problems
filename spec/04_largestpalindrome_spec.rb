b236ada6cf1f23fffba88135f32161352611bc83
Matthew Cowie <matthew@cowie.me>
2013-01-14 18:56:20 -0600

    first crack at #4, getting error on the multiply operation

---
+++ spec/04_largestpalindrome_spec.rb
@@ -0,0 +1,35 @@
+#A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers 
+#is 9009 = 91 × 99.
+
+#Find the largest palindrome made from the product of two 3-digit numbers.
+
+
+class Palindrome
+	def isPalindrome(num)
+    num.to_s.reverse == num.to_s
+  end
+
+  def findLargest(range1)
+    range2 = range1.dup
+    largest = 0
+    range1.each do |x|
+      range2.each do |y|
+        z = x * y
+        if isPalindrome(z) && z > largest
+          largest = z
+        end
+      end
+    end       
+    largest  
+  end
+
+end
+
+describe Palindrome do 
+	it "detects if a number is palindromic" do
+		Palindrome.new.isPalindrome(9009).should == true
+	end
+  it "iterates through the range of three digit numbers and finds the largest palindromic product" do
+    Palindrome.new.findLargest([10..99]).should == 9009 
+	end
+end
